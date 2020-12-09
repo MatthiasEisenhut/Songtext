472060150f9ea603550cfa340910065048571fd4
MatthiasEisenhut <eisenhu.m03@htlwienwest.at>
2020-12-09 12:53:53 +0100

    Stand 09.12.2020, 12:53

--- Songtext/Program.cs
+++ Songtext/Program.cs
@@ -10,60 +10,65 @@ namespace RegexTest
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int songMatcher(StreamReader str, string regex)
         {
+            int anzahl = 0;
             using (StreamReader reader = new StreamReader("../../daten.txt"))
             {
                 string zeile;
-                Regex rx = new Regex("ein");
+                Regex rx = new Regex(regex);
                 while ((zeile = reader.ReadLine()) != null)
                 {
                     MatchCollection matchCollection = rx.Matches(zeile);
-                    System.Diagnostics.Debug.WriteLine(matchCollection.Count);
-                    foreach (Match item in matchCollection)
-                    {
-
-
-                        // nimm das Code-snippet und mache eine Funktion daraus (am besten übergibst Du nur den String der Regular Expression) 
-                        // und dann:
-
-                        // Zähle folgende Strings in der Datei:
-                        // bar
-                        // Bar
-                        // bar oder Bar
-
-                        // alive
-                        // Alive
-                        // alive oder Alive
-
-                        // alle Zeilen die entweder
-                        // You got me rockin and a-rollin
-                        // oder 
-                        //Rockin and a - reelin
-
-                        // "brother" oder "mother" aber sonst nichts
-
-                        // evolution ohne ein "r" davor
-
-                        // "you" oder "you're" aber kein "your"  
-
-                        // know am Ende einer Zeile
-
-                        // wieviele Worte beginnen mit jedem buchstaben des Alphabets- daher Anzahl aller *Worte* die mit "a" beginnen (case-insensitive), b, c, d ...
-
-                        // komplizierter: gib die Zeilen aus die 
-                        // Bands und Liedertitle enthalten
-                        // nur die Bands
-                        // nur die Liedertitel
-                        // die Anzahl der Zeilen jedes Liedtitles
-                    }
-
-
-
+                    anzahl += matchCollection.Count;
+                    //System.Diagnostics.Debug.WriteLine(matchCollection.Count);
+                    //foreach (Match item in matchCollection)
+                    //{
+                    //    output += $"{item};";
+                    //}
                 }
             }
+            return anzahl;
+        }
 
+        static void Main(string[] args)
+        {
+            // nimm das Code-snippet und mache eine Funktion daraus (am besten übergibst Du nur den String der Regular Expression) 
+            // und dann:
 
+            // Zähle folgende Strings in der Datei:
+            // bar
+            // Bar
+            // bar oder Bar
+
+            // alive
+            // Alive
+            // alive oder Alive
+
+            System.Diagnostics.Debug.WriteLine(songMatcher(reader, "bar"));
+            reader.DiscardBufferedData();
+            System.Diagnostics.Debug.WriteLine(songMatcher(reader, "Bar"));
+
+            // alle Zeilen die entweder
+            // You got me rockin and a-rollin
+            // oder 
+            //Rockin and a - reelin
+
+            // "brother" oder "mother" aber sonst nichts
+
+            // evolution ohne ein "r" davor
+
+            // "you" oder "you're" aber kein "your"  
+
+            // know am Ende einer Zeile
+
+            // wieviele Worte beginnen mit jedem buchstaben des Alphabets- daher Anzahl aller *Worte* die mit "a" beginnen (case-insensitive), b, c, d ...
+
+            // komplizierter: gib die Zeilen aus die 
+            // Bands und Liedertitle enthalten
+            // nur die Bands
+            // nur die Liedertitel
+            // die Anzahl der Zeilen jedes Liedtitles
         }
     }
 }
